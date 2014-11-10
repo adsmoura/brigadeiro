b1d51849055e27d6948f6ba3cb45c44d4d7a1402
filip_000 <filip_000@FilipeLeao>
2014-11-10 20:51:38 -0200

    criação do conteudo das classes clientesDAO e ClienteBean

--- src/main/java/br/com/mouralacerda/brigadeiro/DAO/ClienteDAO.java
+++ src/main/java/br/com/mouralacerda/brigadeiro/DAO/ClienteDAO.java
@@ -5,10 +5,43 @@
  */
 package br.com.mouralacerda.brigadeiro.DAO;
 
+import br.com.mouralacerda.brigadeiro.Modelo.Cliente;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+
 /**
  *
  * @author filip_000
  */
 public class ClienteDAO {
+    final EntityManagerFactory factory = Persistence.createEntityManagerFactory("bd_Brigadeiro");
+    final EntityManager em = factory.createEntityManager();
     
+    public void salvar(Cliente c){
+        em.getTransaction().begin();
+        em.persist(c);
+        em.getTransaction().commit();
+    }
+    
+    public Cliente consultarCliente(int codigo){
+        Cliente c = em.find(Cliente.class, codigo);
+        return c;
+    }
+    
+    public List<Cliente> consultarLista(){
+        
+        TypedQuery<Cliente> query = em.createQuery("select c from Cliente c", Cliente.class);
+        List<Cliente> clientes = query.getResultList();
+        return clientes;
+    }
+    
+    public void excluir(Cliente c){
+        em.getTransaction().begin();
+        c = em.merge(c);
+        em.remove(c);
+        em.getTransaction().commit();
+    }
 }
--- src/main/java/br/com/mouralacerda/brigadeiro/bean/ClienteBean.java
+++ src/main/java/br/com/mouralacerda/brigadeiro/bean/ClienteBean.java
@@ -5,10 +5,57 @@
  */
 package br.com.mouralacerda.brigadeiro.bean;
 
+import br.com.mouralacerda.brigadeiro.Modelo.Cliente;
+import br.com.mouralacerda.brigadeiro.DAO.ClienteDAO;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+
 /**
  *
  * @author filip_000
  */
+@ManagedBean
 public class ClienteBean {
     
+    private Cliente c = new Cliente();
+
+    private List<Cliente> clientes = new ArrayList<Cliente>();
+    
+    private final ClienteDAO cDAO = new ClienteDAO();
+
+    public void salvarCliente() {
+        cDAO.salvar(c);
+        c = new Cliente();
+        carregaLista();
+    }
+    
+    public void excluirCliente(Cliente c) {
+        System.out.println("Excluindo");
+        cDAO.excluir(c);
+        System.out.println("Excluido");
+        carregaLista();
+    }
+    
+    @PostConstruct
+    public void carregaLista(){
+        clientes = cDAO.consultarLista();
+    }
+
+    public Cliente getCliente() {
+        return c;
+    }
+
+    public void setCliente(Cliente c) {
+        this.c = c;
+    }
+
+    public List<Cliente> getClientes() {
+        return clientes;
+    }
+
+    public void setClientes(List<Cliente> clientes) {
+        this.clientes = clientes;
+    }
 }
