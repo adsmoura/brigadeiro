219fcdbd02e7294363d312fde37028b54f3ce10c
diegues <diegues.marcos@gmail.com>
2014-11-10 20:46:49 -0200

    metodos na dao produto

--- src/main/java/br/com/mouralacerda/brigadeiro/DAO/ProdutoDAO.java
+++ src/main/java/br/com/mouralacerda/brigadeiro/DAO/ProdutoDAO.java
@@ -5,10 +5,34 @@
  */
 package br.com.mouralacerda.brigadeiro.DAO;
 
+import br.com.mouralacerda.brigadeiro.Modelo.Produto;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+
 /**
  *
  * @author filip_000
  */
 public class ProdutoDAO {
+    final EntityManagerFactory factory = Persistence.createEntityManagerFactory("db_Brigadeiro");
+    final EntityManager em = factory.createEntityManager();
     
+    public void salvar(Produto p){
+        em.getTransaction().begin();
+        em.persist(p);
+        em.getTransaction().commit();
+    }
+    
+    public List<Produto> ListarProdutos(){
+        TypedQuery<Produto> query = em.createQuery("Select p from Produto p", Produto.class);
+        List<Produto> Produto = query.getResultList();
+        return Produto;
+    }
+    public void excluir(){
+        
+    }
 }
+
